df7d3d74f31b16486b5539e06c4681b6f7a670b1
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2024-03-04 22:55:42 -0300

    feat: add getAll for history service

--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class HistoryService {}
+export class HistoryService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAll() {
+    try {
+      return this.prisma.log.findMany();
+    } catch (error) {
+      console.error('An error has ocurred while fetching data: ', error);
+      throw new Error(`An error has ocurred white fetching data : ${error}`);
+    }
+  }
+}
