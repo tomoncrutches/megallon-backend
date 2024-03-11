81c34f759825981a6a63d4374e879e43fc902b3a
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2024-03-11 11:59:35 -0300

    feat: implements history service in material controller for create log

--- src/material/material.controller.ts
+++ src/material/material.controller.ts
@@ -13,16 +13,26 @@ import {
 
 import { Material } from '@prisma/client';
 import { MaterialService } from './material.service';
+import { HistoryService } from 'src/history/history.service';
 
 @Controller('material')
 export class MaterialController {
-  constructor(private readonly service: MaterialService) {}
+  constructor(
+    private readonly service: MaterialService,
+    private readonly historyService: HistoryService,
+  ) {}
   private readonly logger = new Logger('MaterialController');
 
   @Post()
   async create(@Body() data: Material) {
     try {
-      return await this.service.create(data);
+      const material = await this.service.create(data);
+      await this.historyService.create({
+        action: 'Nueva materia prima',
+        description: `Se llevo a cabo la creación de la materia prima ${data.name}.`,
+        user_id: '1d6f37dc-06c7-4510-92e8-a7495e287708',
+      });
+      return material;
     } catch (error) {
       this.logger.error(error.message);
       throw error;
@@ -57,7 +67,13 @@ export class MaterialController {
     try {
       if (!('id' in data)) throw new ForbiddenException('ID is required.');
 
-      return await this.service.update(data);
+      const material = await this.service.update(data);
+      await this.historyService.create({
+        action: 'Actualizar materia prima',
+        description: `Se llevó a cabo la actualización de la materia prima ${data.name}.`,
+        user_id: '1d6f37dc-06c7-4510-92e8-a7495e287708',
+      });
+      return material;
     } catch (error) {
       this.logger.error(error.message);
       throw error;
@@ -70,7 +86,13 @@ export class MaterialController {
     try {
       if (!id) throw new ForbiddenException('ID is required.');
 
-      return await this.service.delete(id);
+      const material = await this.service.delete(id);
+      await this.historyService.create({
+        action: 'Eliminar materia prima',
+        description: `Se llevó a cabo eliminación de la materia prima ${material.name}.`,
+        user_id: '1d6f37dc-06c7-4510-92e8-a7495e287708',
+      });
+      return material;
     } catch (error) {
       this.logger.error(error.message);
       throw error;
--- src/material/material.module.ts
+++ src/material/material.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { MaterialService } from './material.service';
+import { HistoryService } from 'src/history/history.service';
 import { MaterialController } from './material.controller';
+import { MaterialService } from './material.service';
+import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [MaterialService, PrismaService],
-  controllers: [MaterialController]
+  providers: [MaterialService, PrismaService, HistoryService],
+  controllers: [MaterialController],
 })
 export class MaterialModule {}
