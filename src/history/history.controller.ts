3669d43c0daa20997a8a481a4ba08f808e76429e
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2024-03-11 10:59:03 -0300

    fix: delete post endpoint in history controller

--- src/history/history.controller.ts
+++ src/history/history.controller.ts
@@ -1,19 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
 import { HistoryService } from './history.service';
-import { Log } from '@prisma/client';
 
 @Controller('history')
 export class HistoryController {
   constructor(private readonly service: HistoryService) {}
-
-  @Post()
-  async create(@Body() data: Log) {
-    try {
-      return await this.service.create(data);
-    } catch (error) {
-      throw error;
-    }
-  }
 
   @Get()
   async getAll() {
