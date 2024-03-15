c1528c1d837b2ab1b7ed5026cd17e62223d2e691
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2024-03-15 16:37:09 -0300

    feat: cloudinary image upload when create a material

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
