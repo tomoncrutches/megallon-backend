4a73553a7ffca61d9ba0cd9c665156306fd8a948
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2024-04-04 18:42:27 -0300

    fix: port is defined in enviroment variables file

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { NestFactory } from '@nestjs/core';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
