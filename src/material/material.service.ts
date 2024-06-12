1386484941b515a3d2688e4415aeaa0a4082e135
Gianluca Bredice <bredicegian@gmail.com>
2024-06-12 14:09:58 -0300

    fix: get dynamic material items that they are "etiquetas" for use on consumePackagin function on material service

--- src/material/material.service.ts
+++ src/material/material.service.ts
@@ -10,17 +10,6 @@ import { Material } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { TransactionService } from 'src/transaction/transaction.service';
 
-const etiquetaSabor = {
-  NOA: '7ff29ac5-e9e1-4563-92cf-ac5afccdd8bd',
-  'NOA S/SAL': '6b6c9711-fdcd-4230-b4cb-5d1ce12f1df3',
-  Arvejas: '13993496-f3be-4b48-b0a4-a01486dd3c65',
-  Lentejas: 'cf782a6b-8b36-4822-91d3-69392b1352b2',
-  Carbonada: 'b008911d-79e1-4bdf-afad-7f84b81dd57b',
-  'Risotto de Hongos': '98f5a47f-8e98-43b1-9c77-2b16401a3638',
-  Mediterráneo: 'fd162326-2b87-4236-bd29-667b484cf798',
-  'Poroto Aduki': 'b15f09e8-7dea-4fa6-9a47-f913337a4483',
-  Bruselas: 'e771f06e-a7d5-41a5-b89f-71593f2a523f',
-};
 @Injectable()
 export class MaterialService {
   constructor(
@@ -129,7 +118,7 @@ export class MaterialService {
     }
   }
 
-  async consumePackaging(quantity, name): Promise<void> {
+  async consumePackaging(quantity: number, name: string): Promise<void> {
     try {
       //Descuento de separadores por paquete
       await this.prisma.material.update({
@@ -141,10 +130,29 @@ export class MaterialService {
         where: { id: 'e167f44a-ebd1-4bac-9dbd-8b505449b99f' },
         data: { stock: { decrement: quantity } },
       });
+
+      //Obtiene los materiales "Etiquetas"
+      const labels = await this.getLabels();
+
       //etiqueta por paquete
       await this.prisma.material.update({
-        where: { id: etiquetaSabor[name] },
+        where: {
+          id: labels.find((l) =>
+            l.name.toLowerCase().includes(name.toLowerCase()),
+          ).id,
+        },
         data: { stock: { decrement: quantity } },
+      });
+    } catch (error) {
+      this.logger.error(error.message);
+      throw error;
+    }
+  }
+
+  private async getLabels(): Promise<Material[]> {
+    try {
+      return await this.prisma.material.findMany({
+        where: { name: { contains: 'Etiquetas' } },
       });
     } catch (error) {
       this.logger.error(error.message);
